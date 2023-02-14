24fd74427cbea3cad9ae0b9841f2b0cf92a0b266
RenanMoreir <renanrodriguesmoreira@hotmail.com>
2023-02-14 11:00:55 -0300

    meu primeiro programa PHP

---
+++ aula1/ola2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- aqui é o codigo HTML -->
+    <h1>Exemplo olá mundo</h1>
+
+    <?php
+        //Aqui é o codigo PHP
+        echo "<h2>olá IFSP </h2>"
+    ?>
+
+
+</body>
+</html>
