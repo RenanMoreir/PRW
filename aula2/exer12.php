c3fa109f7289d712183f0b9e063d5505ba93cb4b
Renan <renanrodriguesmoreira@hotmail.com>
2023-02-28 12:26:18 -0300

    exercicio 12

--- aula2/exer12.php
+++ aula2/exer12.php
@@ -11,5 +11,18 @@ $cir = 3.14159*($c*$c);
 $cir=number_format($cir, 3, ".", "");
 echo "CIRCULO: $cir\n";
 
+$tra = ($a + $b)*$c/2;
+$tra=number_format($tra, 3, ".", "");
+echo "TRAPEZIO: $tra\n";
+
+$qua = $b*$b;
+$qua=number_format($qua, 3, ".", "");
+echo "QUADRADO: $qua\n";
+
+$ret = $a*$b;
+$ret=number_format($ret, 3, ".", "");
+echo "RETANGULO: $ret\n";
+
+
 
 ?>
